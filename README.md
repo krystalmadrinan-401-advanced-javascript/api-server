c1bdf1919e4c9e02c48f91e01085a00174b02e5d
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2020-06-17 23:52:09 -0700

    what I have for week 2 labs for now

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # api-server
 
+
+## Resources & Collaborations
+- https://www.npmjs.com/package/json-server
+- Worked on it with David P.
+
 ## Dynamic API: Phase 1
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 'use strict';
 
 const jsonServer = require('json-server');
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 3001;
+
+server.use(middlewares)
+server.use(router)
+
+// ACTUALLY GET JSON
+let data = require('./data/db.json');
+
+// ROUTES
+
+server.get('/', function (req, res) {
+    res.send('TO home');
+});
+
+server.get('/categories', function catHandler (req, res) {
+    
+    res.send(data.categories);
+});
+
+
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
